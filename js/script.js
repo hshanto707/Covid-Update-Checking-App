e6e697162d75d24b2be5c919e85aa49facf1a8fc
unknown <hshanto707>
2021-08-30 00:22:41 +0600

    made few adjustments

--- js/script.js
+++ js/script.js
@@ -21,7 +21,6 @@ const loadCovidData = () => {
 };
 
 const displayWeatherData = (data) => {
-  console.log(data);
   if (data.message == "Country not found or doesn't have any cases") {
     document.getElementById('error-message').style.display = 'block';
     document.getElementById('main-display').style.display = 'none';
